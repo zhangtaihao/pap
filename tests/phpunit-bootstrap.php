6e399503aa7593c88ee7b4d0df18074241917791
Taihao Zhang <jason@zth.me>
2012-05-17 17:34:39 +1000

    Defined testing directory instead of customizing the configuration root.

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -4,8 +4,8 @@
  * Bootstrap file for PHPUnit.
  */
 
-// Set test directory as configuration root.
-define('CONF_ROOT', dirname(__FILE__));
-
 // Initialize default paths.
 require_once '../web/init.default.php';
+
+// Declare the tests directory.
+define('TEST_ROOT', dirname(__FILE__));
