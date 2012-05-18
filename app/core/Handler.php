1ad0fca2e9271bae00e0a21b5f1851022785bfc3
Taihao Zhang <jason@zth.me>
2012-05-19 00:19:13 +1000

    Moved handler implementation to its own file.

---
+++ app/core/Handler.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Application handler mechanism architecture.
+ */
+
+/**
+ * Generic application handler.
+ */
+class ApplicationHandler {
+  /**
+   * Application context.
+   * @var ApplicationContext
+   */
+  protected $context;
+
+  /**
+   * Instantiates an application with the specified configuration.
+   *
+   * @param string $handler
+   *   Configured handler name.
+   * @param ApplicationContext $context
+   *   Full path to the configuration file.
+   */
+  public function __construct($handler, ApplicationContext $context) {
+    $this->context = $context;
+    $this->initialize();
+  }
+
+  /**
+   * Initializes the application.
+   */
+  protected function initialize() {
+    // TODO
+  }
+
+  /**
+   * Runs the application.
+   */
+  public function run() {
+    // TODO
+  }
+}
