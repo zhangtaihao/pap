b29ce85fca2747c115141e070f1c3f3e731e06e8
Taihao Zhang <jason@zth.me>
2012-05-20 23:27:28 +1000

    Added the application libraries directory.

---
+++ app/lib/README.txt
@@ -0,0 +1,2 @@
+This directory contains PHP libraries distributed with the application. Place
+library folders in here for the application to find.
--- web/init.default.php
+++ web/init.default.php
@@ -11,17 +11,6 @@ if (!defined('PLATFORM_ROOT')) {
   define('PLATFORM_ROOT', realpath(dirname(__FILE__) . '/..'));
 }
 
-if (!defined('LIB_ROOT')) {
-  /**
-   * Libraries directory.
-   */
-  define('LIB_ROOT', PLATFORM_ROOT . '/lib');
-}
-if (is_dir(LIB_ROOT)) {
-  // Set up libraries for include.
-  ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . LIB_ROOT);
-}
-
 if (!defined('WEB_ROOT')) {
   /**
    * Web directory.
@@ -42,3 +31,18 @@ if (!defined('CONF_ROOT')) {
    */
   define('CONF_ROOT', PLATFORM_ROOT . '/conf');
 }
+
+if (!defined('LIB_ROOT')) {
+  /**
+   * Libraries directory.
+   */
+  define('LIB_ROOT', PLATFORM_ROOT . '/lib');
+}
+if (!defined('APPLIB_ROOT')) {
+  /**
+   * Directory for libraries included with the application.
+   */
+  define('APPLIB_ROOT', APP_ROOT . '/lib');
+}
+// Set up libraries for include.
+ini_set('include_path', LIB_ROOT . PATH_SEPARATOR . APPLIB_ROOT . PATH_SEPARATOR . ini_get('include_path'));
